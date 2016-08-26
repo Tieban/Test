85df28c67ac7d999e325a61290d586b90fa48242
Nick <mliuhangyu@163.com>
2016-08-27 01:56:52 +0800

    【ADD】Test：新增index.php模块

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 2016/8/26
+ * Time: 23:51
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1><?php echo "Hello World!";?></h1>
+
+
+
+</body>
+</html>
